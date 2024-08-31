6d80235202a70f85511106d5d3f475f5a42657e1
amit-tiwari09 <amit067843@gmail.com>
2024-08-31 17:12:07 +0545

    models made Authenticatable

--- app/applicant.php
+++ app/applicant.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class applicant extends Model
+class Applicant extends Model
 {
-    protected $guarded = ["id"];
+   protected $guarded = ["id"];
 }
